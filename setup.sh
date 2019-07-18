17d3fcb199c6ef68ba87fceebcfff51ce8be3c0e
sumancvb <sumancvb@gmail.com>
2019-07-18 23:43:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "****************************************************"
 echo "Creating timelapse-pi service"
 echo "****************************************************"
+
 cat > /etc/systemd/system/timelapse-pi.service <<'EOF'
 [Unit]
 Description=Raspistill Timelapse
@@ -26,12 +27,23 @@ EOF
 cat > /home/pi/timelapse.sh <<'EOF'
 #!/bin/bash
 #raspistill -t 43200000 -tl 20000 -o /media/pi/NOX/image%06d.jpg
+sleep 30
 while :
 do
   DATE=$(date +"%Y-%m-%d_%H%M%S")
-  raspistill -vf -hf -o /media/pi/NOX/$DATE.jpg
+  raspistill -vf -hf -o /media/pi/NOX/timelapse/$DATE.jpg
   sleep 30
 done
 EOF
 
 sudo chmod +x /home/pi/timelapse.sh
+
+sudo systemctl daemon-reload 
+sudo systemctl enable timelapse-pi.service
+sudo systemctl start timelapse-pi.service
+
+echo "****************************************************"
+echo "DONE !"
+echo "****************************************************"
+
+sudo systemctl status timelapse-pi.service
